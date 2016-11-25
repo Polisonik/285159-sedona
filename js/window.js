c18b3b8441ff43b6d820e18f79c9712e80bd0ffb
Polisonik <allred@inbox.ru>
2016-11-25 21:04:17 +0300

    Update window.js

--- js/window.js
+++ js/window.js
@@ -1,8 +1,9 @@
-var link=document.querySelector(".btn");
-var popup = document.querySelector(".modal-window");
-popup.classList.toggle("modal-window-close");
-
-link.addEventListener("click", function(event){
-event.preventDefault();
-popup.classList.toggle("modal-window-close");
-});
+  var link=document.querySelector(".btn");
+  var popup = document.querySelector(".modal-window");
+  
+  popup.classList.toggle("modal-window-close");
+  link.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.toggle("modal-window-close");
+  });
+ 
