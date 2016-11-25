8c475e55508e834cb89f71fda8feed00c9fd21e7
Polisonik <allred@inbox.ru>
2016-11-25 20:43:02 +0300

    На проверку 3 раз

--- js/window.js
+++ js/window.js
@@ -1,9 +1,8 @@
-  var link=document.querySelector(".btn");
-  var popup = document.querySelector(".modal-window");
-  
+var link=document.querySelector(".btn");
+var popup = document.querySelector(".modal-window");
+popup.classList.toggle("modal-window-close");
 
-  link.addEventListener("click", function(event){
-    event.preventDefault();
-    popup.classList.toggle("modal-window-close");
-  });
- 
+link.addEventListener("click", function(event){
+event.preventDefault();
+popup.classList.toggle("modal-window-close");
+});
